d101694e80dfcf9252df4b53864d184623024c93
asdas <sanjaydhfhg234@gmail.com>
2025-09-02 12:34:19 +0000

    no tab is working check the backend

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/personalized-diet-plan-generation.ts';
-import '@/ai/flows/risk-prediction-explanation.ts';
-import '@/ai/flows/symptom-checker-disease-suggestions.ts';
-import '@/ai/flows/urgency-assessment-next-steps.ts';
+import './flows/personalized-diet-plan-generation';
+import './flows/risk-prediction-explanation';
+import './flows/symptom-checker-disease-suggestions';
+import './flows/urgency-assessment-next-steps';
